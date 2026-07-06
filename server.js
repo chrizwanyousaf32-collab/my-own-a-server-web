38dcec7a30dc53a09770fe87bbb6a64b57e6ecc6
Your Name <youremail@example.com>
2026-07-06 10:42:32 -0700

    Added Cloudinary upload

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 import express from "express";
+import multer from "multer";
 
 const app = express();
 
-app.get("/",(req,res)=> {
-    res.json("I AM IBRAHIM ")
-})
- 
-app.get("/profile/:username",(req,res)=>{
-    res.json(req.params)
-})
+const upload = multer({ dest: "uploads/" });
 
-const port = process.env.PORT || 3000;
-app.listen (port, () =>{
-    console.log("server is rady")
+app.use("/uploads", express.static("uploads"));
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Upload a Photo</h1>
+
+    <form action="/upload" method="POST" enctype="multipart/form-data">
+      <input type="file" name="photo">
+      <button>Upload</button>
+    </form>
+  `);
+});
+
+app.post("/upload", upload.single("photo"), (req, res) => {
+  res.send(`
+    <h2>Photo Uploaded!</h2>
+
+    <img src="/uploads/${req.file.filename}" width="300">
+
+    <br><br>
+
+    <a href="/">Upload Another</a>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log("Server running...");
 });
